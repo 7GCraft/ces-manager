2c38041fcbfc0168a653006ded883e31e3ed53b5
Phobez <36761666+Phobez@users.noreply.github.com>
2021-02-04 14:04:59 +0700

    Updated resourceTiersServices.js

--- src/models/resourceTierModel.js
+++ src/models/resourceTierModel.js
@@ -1,7 +1,8 @@
 module.exports = class ResourceTier {
-    constructor(ResourceTierID, ResourceTierName, ResourceTierTradePower) {
+    constructor(ResourceTierID, ResourceTierName, ResourceTierTradePower, Resources) {
         this.ResourceTierID = ResourceTierID;
         this.ResourceTierName = ResourceTierName;
         this.ResourceTierTradePower = ResourceTierTradePower;
+        this.Resources = Resources;
     }
 }
--- src/services/resourceTiersServices.js
+++ src/services/resourceTiersServices.js
@@ -1,5 +1,6 @@
 const path = require('path');
-const ResourceTier = require('../models/resourceTierModel')
+const ResourceTier = require('../models/resourceTierModel');
+const Resource = require('../models/resourceModel')
 const knex = require('knex')({
     client: 'sqlite3',
     connection: {
@@ -21,13 +22,41 @@ const getAllResourceTiers = async function() {
     return resourceTiers;
 };
 
-const getResourceTier = async function(id) {
-    let rawResourceTier = await knex.select('*').from('ResourceTier').where('resourceTierId', id);
+// const getResourceTier = async function(id) {
+//     let rawResourceTier = await knex.select('*').from('ResourceTier').where('resourceTierId', id);
 
-    let resourceTier = new ResourceTier(rawResourceTier.resourceTierId, rawResourceTier.name, rawResourceTier.tradePower);
+//     let resourceTier = new ResourceTier(rawResourceTier.resourceTierId, rawResourceTier.name, rawResourceTier.tradePower);
 
-    return resourceTier;
-}
+//     return resourceTier;
+// }
 
-exports.getAllResourceTiers = getAllResourceTiers;
-exports.getResourceTier = getResourceTier;
+const getResourceTiers = async function() {
+    let rawResourceTiers = await knex.select('*').from('ResourceTier');
+
+    let rawResources = await knex.select('*').from('Resource');
+    
+    let resourceTiers = [];
+
+    for (let rawResourceTier of rawResourceTiers) {
+        let resources = [];
+        let resourceTier = new ResourceTier(rawResourceTier.resourceTierId, rawResourceTier.name, rawResourceTier.tradePower, resources);
+
+        resourceTiers.push(resourceTier);
+    }
+
+    for (let rawResource of rawResources) {
+        let resource = new Resource(rawResource.resourceId, rawResource.name, rawResource.resourceTierId);
+
+        resourceTiers[resource.ResourceTierID - 1].Resources.push(resource);
+    }
+
+    // let rawResources = await knex.select('Resource.resourceId', 'Resource.name as resourceName', 'Resource.resourceTierId', 'ResourceTier.name as resourceTierName', 'ResourceTier.tradePower').from('Resource').leftJoin('ResourceTier', 'Resource.resourceTierId', 'ResourceTier.resourceTierId');
+
+    // console.log(rawResources);
+    console.info(resourceTiers);
+};
+
+// exports.getAllResourceTiers = getAllResourceTiers;
+// exports.getResourceTier = getResourceTier;
+
+getResourceTiers();
