754a42b5d40b7d605aa5ed731f9eea183a230062
Phobez <36761666+Phobez@users.noreply.github.com>
2021-02-15 15:08:05 +0700

    Added getComponentFunctionalByRegionId function.

--- src/services/componentServices.js
+++ src/services/componentServices.js
@@ -123,6 +123,70 @@ const getComponentByFacilityId = async (id) => {
 }
 
 /**
+ * Gets all functional components of a given region id.
+ * @param {Number} id must be an integer.
+ * @returns {Array} array of component objects if successful, null otherwise. 
+ */
+const getComponentFunctionalByRegionId = async (id) => {
+    const rawComponents = await knex
+        .select(constants.TABLE_COMPONENT + '.' + '*')
+        .from(constants.TABLE_COMPONENT)
+        .innerJoin(
+            constants.TABLE_FACILITY,
+            constants.TABLE_COMPONENT + '.' + constants.COLUMN_FACILITY_ID,
+            constants.TABLE_FACILITY + '.' + constants.COLUMN_FACILITY_ID
+        )
+        .where(constants.TABLE_COMPONENT + '.' + constants.COLUMN_REGION_ID, id)
+        .andWhere(constants.TABLE_FACILITY + '.' + constants.COLUMN_IS_FUNCTIONAL, 1)
+        .catch(e => {
+            console.error(e);
+        });
+
+    const componentTypes = await getComponentTypeAll();
+    
+    const resources = await resourceServices.getResourceAll();
+    
+    if (rawComponents.length === 0 || componentTypes === null || resources === null) return null;
+
+    let components = [];
+
+    for (let rawComponent of rawComponents) {
+        let componentValue = rawComponent.value;
+
+        if (rawComponent.componentTypeId === 3) {
+            componentValue = resources[parseInt(rawComponent.value.split(';')[1]) - 1];
+        }
+
+        let component = new Component(
+            rawComponent.componentId,
+            rawComponent.name,
+            componentTypes[rawComponent.componentTypeId - 1],
+            rawComponent.regionId,
+            rawComponent.facilityId,
+            componentValue,
+            rawComponent.activationTime,
+            rawComponent.isChild,
+            rawComponent.parentId
+        );
+
+        components.push(component);
+    }
+
+    for (let component of components) {
+        if (component.isChild) {
+            for (let parentComponent of components) {
+                if (component.parentId === parentComponent.componentId) {
+                    component.parent = parentComponent;
+                    break;
+                }
+            }
+        }
+    }
+
+    return components;
+}
+
+/**
  * Creates a new component.
  * @param {Component} component must be a component object.
  * @returns {Boolean} true if successful, false otherwise.
@@ -278,6 +342,7 @@ const getComponentTypeAll = async () => {
 
 exports.getComponentByRegionId = getComponentByRegionId;
 exports.getComponentByFacilityId = getComponentByFacilityId;
+exports.getComponentFunctionalByRegionId = getComponentFunctionalByRegionId;
 exports.addComponent = addComponent;
 exports.updateComponent = updateComponent;
 exports.deleteComponentById = deleteComponentById;
@@ -289,3 +354,4 @@ exports.getComponentTypeAll = getComponentTypeAll;
 // getComponentTypeAll().then(data => console.log(data));
 // deleteComponentById(5)
 //     .then(data => console.log(data));
+// getComponentUsedByRegionId(1).then(data => console.log(data));
