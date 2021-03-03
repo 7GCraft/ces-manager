4b36d96ab54b01b0959bbec29a9cc759c89de684
DeathWish99 <DeathWish99@users.noreply.github.com>
2021-03-03 13:30:15 +0700

    Tables sortable

--- src/renderers/PartialViewRenderers/region/componentsInfo.js
+++ src/renderers/PartialViewRenderers/region/componentsInfo.js
@@ -398,6 +398,75 @@ function setComponentList(res) {
     }
 }
 
+function sortTable(index){
+    let table = document.getElementById("tblComponents");
+    let switching = true;
+    let dir = "asc";
+    let switchCount = 0;
+
+    while(switching){
+        let shouldSwitch = false;
+        switching = false;
+        rows = table.rows;
+
+        for(i = 1; i < (rows.length - 1); i++){
+            shouldSwitch = false;
+
+            x = rows[i].getElementsByTagName("TD")[index];
+            y = rows[i + 1].getElementsByTagName("TD")[index];
+
+            if(index != 0){
+                if (dir == "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                      // If so, mark as a switch and break the loop:
+                      shouldSwitch = true;
+                      break;
+                    }
+                } 
+                else if (dir == "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                      // If so, mark as a switch and break the loop:
+                      shouldSwitch = true;
+                      break;
+                    }
+                }
+            }
+            else{
+                if (dir == "asc") {
+                    if (Number(x.innerHTML) > Number(y.innerHTML)) {
+                      // If so, mark as a switch and break the loop:
+                      shouldSwitch = true;
+                      break;
+                    }
+                } 
+                else if (dir == "desc") {
+                    if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                      // If so, mark as a switch and break the loop:
+                      shouldSwitch = true;
+                      break;
+                    }
+                }
+            }
+        }
+
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchCount ++;
+          } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchCount == 0 && dir == "asc") {
+              dir = "desc";
+              switching = true;
+            }
+        }
+    }
+}
+
 function populateUpdateComponentForm(componentId) {
     let splicedComponentId = componentId.replace('Component', '');
     let facilityId = $('#' + componentId).data("facilityId");
