b45110ea7f311e3967230a814b2428e445565d6f
Anthony GT <anthonygt124@gmail.com>
2021-11-05 16:55:27 +0700

    Display used population (Issue #44)

--- src/models/regionListItemModel.js
+++ src/models/regionListItemModel.js
@@ -9,15 +9,17 @@ module.exports = class RegionListItem {
      * @param {Number} RegionTotalFood must be an integer.
      * @param {String} StateName must be a string.
      * @param {Number} Population must be an integer.
+     * @param {Number} UsedPopulation must be an integer
      * @param {Number} DevelopmentId must be an integer.
      */
-    constructor(RegionID, RegionName, RegionTotalIncome, RegionTotalFood, StateName, Population, DevelopmentId) {
+    constructor(RegionID, RegionName, RegionTotalIncome, RegionTotalFood, StateName, Population, UsedPopulation, DevelopmentId) {
         this.RegionID = RegionID;
         this.RegionName = RegionName;
         this.RegionTotalIncome = RegionTotalIncome;
         this.RegionTotalFood = RegionTotalFood;
         this.StateName = StateName;
         this.Population = Population;
+        this.UsedPopulation = UsedPopulation;
         this.DevelopmentId = DevelopmentId;
     }
 }
