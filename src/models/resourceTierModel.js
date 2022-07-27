55781bd48124a554aa094f6e525c8c005dbdd599
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-07-27 14:39:46 +0800

    Finish setup the vue-electron scaffold.  (#102)
    * Finish setup the vue-electron scaffold.
    
    * Add scripts to make starting the app easier.

--- src/models/resourceTierModel.js
+++
@@ -1,8 +0,0 @@
-module.exports = class ResourceTier {
-    constructor(ResourceTierID, ResourceTierName, ResourceTierTradePower, Resources) {
-        this.ResourceTierID = ResourceTierID;
-        this.ResourceTierName = ResourceTierName;
-        this.ResourceTierTradePower = ResourceTierTradePower;
-        this.Resources = Resources;
-    }
-}
