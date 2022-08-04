035d58e0858590af6ff20decfc65cab7b8dc7f39
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-08-04 11:07:03 +0800

    Setup the vue-electron scaffold with vue2.

--- src/models/corruptionModel.js
+++
@@ -1,7 +0,0 @@
-module.exports = class Corruption {
-    constructor(corruptionId, corruptionName, corruptionRate) {
-        this.corruptionId = corruptionId;
-        this.corruptionName = corruptionName;
-        this.corruptionRate = corruptionRate;
-    }
-}
