a0f0e9279934e727fbcdd34fa9b08c8dafcefe75
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-08-11 10:55:09 +0800

    Add a fully dynamic button.
    Add a fully dynamic button using bootstrap-vue.

---
+++ src/shared/UIElements/Button/Button.vue
@@ -0,0 +1,27 @@
+<template>
+  <b-button
+    :variant="type"
+    :size="size"
+    :disabled="!state"
+    v-on:click="onclick"
+    :id="id"
+    ><slot/></b-button
+  >
+</template>
+
+<script>
+export default {
+  name: "Button",
+  components: {},
+  props: {
+    id:String,
+    onclick: Function,
+    type: String,
+    state: Boolean,
+    size: String,
+    name: String,
+  },
+};
+</script>
+
+<style></style>
