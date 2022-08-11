707e0eeddb9e6026a2b6e867c87d90d896c3b7f5
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-08-11 10:35:20 +0800

    Add a fully dynamic table component.

---
+++ src/shared/UIElements/Table/Table.vue
@@ -0,0 +1,43 @@
+<template>
+  <div :id="id">
+    <b-row class="m-2">
+    <b-col sm="4">
+    <div class="input-group mb-3">
+      <input v-model="filter" type="text" class="form-control" placeholder="Search" aria-label="filter" >
+    </div>
+    </b-col>
+    </b-row>
+    <b-row class="m-2">
+    <b-col sm="12">
+    <b-table hover
+    head-row-variant="dark"
+    :items="items" 
+    :fields="fields"
+    :filter="filter"
+    label-sort-asc=""
+    label-sort-desc=""
+    label-sort-clear=""
+    ></b-table>
+    </b-col>
+    </b-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Table",
+  components: {},
+  props: {
+    items: Array,
+    fields: Array,
+    id: String,
+  },
+  data(){
+    return{
+        filter: ''
+    }
+  }
+};
+</script>
+
+<style></style>
