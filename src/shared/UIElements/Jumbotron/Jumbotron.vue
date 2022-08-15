f6467d72980d4f5ab9191d13973d16510e329676
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-08-15 09:50:43 +0800

    Implement jumbotron component

---
+++ src/shared/UIElements/Jumbotron/Jumbotron.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-jumbotron
+    lead="Bootstrap v4 Components for Vue.js 2"
+    :bg-variant="type.background"
+    :border-variant="type.border"
+    :text-variant="type.text"
+    :header="header"
+  >
+    <slot />
+  </b-jumbotron>
+</template>
+
+<script>
+export default {
+  name: "Jumbotron",
+  props: {
+    type: Object,
+    header: String,
+  },
+};
+</script>
+
+<style></style>
