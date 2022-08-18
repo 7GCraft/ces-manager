db0a01050f9a17b83a52ec8f5132c1e9946bfbc5
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-08-18 10:27:42 +0800

    Add dynamic modal using bootstrap-vue.

---
+++ src/shared/UIElements/Modal/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+  <b-modal
+    :id="id"
+    :title="title"
+    v-model="showModal"
+    :header-bg-variant="styling.headerBackground || ''"
+    :header-text-variant="styling.headerText || ''"
+    :body-bg-variant="styling.bodyBackground || ''"
+    :body-text-variant="styling.bodyText || ''"
+    :footer-bg-variant="styling.footerBackground || ''"
+    :footer-text-variant="styling.footerText || ''"
+    :sizing="styling.size || ''"
+  >
+    <slot />
+  </b-modal>
+</template>
+
+<script>
+export default {
+  name: "Modal",
+  props: {
+    show: Boolean,
+    id: String,
+    title: String,
+    styling: Object,
+  },
+  data(){
+    return{
+        showModal: this.show
+    }
+  },
+  components: {},
+};
+</script>
+
+<style></style>
