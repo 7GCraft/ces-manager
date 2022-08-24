55429bbb73a50610f47696fcaf10b292c3e97c40
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-08-24 12:43:08 +0800

    Add tab component using bootstrap-vue.

---
+++ src/shared/UIElements/Tabs/Tabs.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="tab">
+    <b-tabs
+      class=""
+      content-class="mt-3"
+      :fill="fill || false"
+      :pills="pill || false"
+      :align="align || 'left'"
+      :end="end || false"
+    >
+      <b-tab
+        :key="item.title"
+        v-for="item in items"
+        :title="item.title"
+        :active="item.active || false"
+      >
+        {{ item.content }}
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tabs",
+  components: {},
+  props: {
+    items: Array,
+    fill: Boolean,
+    pill: Boolean,
+    align: String,
+    end: Boolean,
+  },
+};
+</script>
+
+<style></style>
