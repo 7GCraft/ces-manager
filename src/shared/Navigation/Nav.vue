48feca28a447237a2f95f6eaf492459ee68d933a
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-09-17 15:10:50 +0800

    Add fully dynamic nav component

---
+++ src/shared/Navigation/Nav.vue
@@ -0,0 +1,23 @@
+<template>
+    <b-nav id="nav" :vertical="vertical || false" :class="styling" :align="align || ''">
+        <b-nav-item v-for="item in items" :active="item.active" v-bind:key="item.text">
+            {{item.text}}
+        </b-nav-item>
+    </b-nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Nav',
+    props:{
+        items: Array,
+        vertical: Boolean,
+        styling: String
+    }
+  }
+  </script>
+  
+  <style>
+
+  </style>
+  
