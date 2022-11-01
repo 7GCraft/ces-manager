cffd3c6dabff9498ef69e25b59fca797df4ec83a
Wryan Cartie Halim <33276404+WryanCartie@users.noreply.github.com>
2022-11-01 19:20:37 +0800

    Add all templates to the list.

--- src/renderers/PartialViewRenderers/region/facilityTemplate.js
+++ src/renderers/PartialViewRenderers/region/facilityTemplate.js
@@ -102,10 +102,190 @@ const facilityTemplates = [
         "money": 200
     },
     {
-        "name": "Arena",
+        "name": "Garden",
+        "population": 1,
+    },
+    {
+        "name": "Market",
         "population": 3,
+        "money": 500
+    },
+    {
+        "name": "Food Market",
+        "population": 3,
+        "money": 200,
+        "food": 10
+    },
+    {
+        "name": "Slave Market",
+        "population": 3,
+        "money": 800
+    },
+    {
+        "name": "Charcoal Kiln",
+        "population": 5,
         "money": 600
     },
+    {
+        "name": "Pewter Maker",
+        "population": 3,
+        "money": 350
+    },
+    {
+        "name": "Monument",
+        "population": 1,
+    },
+
+    {
+        "name": "Pleasure House",
+        "population": 2,
+        "money": 600
+    },
+    {
+        "name": "Charcoal Kiln",
+        "population": 5,
+        "money": 600
+    },
+    {
+        "name": "Library",
+        "population": 1,
+      
+    },
+    {
+        "name": "University",
+        "population": 3,
+      
+    },
+    {
+        "name": "School",
+        "population": 5,
+      
+    },
+    {
+        "name": "Granary",
+        "population": 2,
+        "food": 5
+      
+    },
+    {
+        "name": "Canal",
+        "population": 1,
+      
+    },
+    {
+        "name": "Goldsmith",
+        "population": 2,
+        "money":600
+      
+    },
+    {
+        "name": "Silversmith",
+        "population": 2,
+        "money":500
+      
+    },
+    {
+        "name": "Quartzsmith",
+        "population": 2,
+        "money":400
+    },
+    {
+        "name": "Diamond Mine",
+        "population": 5,
+    },
+    {
+        "name": "Emerald Mine",
+        "population": 5,
+    },
+    {
+        "name": "Ruby Mine",
+        "population": 5,
+    },
+    {
+        "name": "Gold Mine",
+        "population": 5,
+    },
+    {
+        "name": "Silk Plantation",
+        "population": 5,
+    },
+    {
+        "name": "Spices Plantation",
+        "population": 5,
+    },
+    {
+        "name": "Prismarine Quarry",
+        "population": 5,
+    },
+    {
+        "name": "Pearl Farm",
+        "population": 5,
+    },
+    {
+        "name": "Silver Mine",
+        "population": 5,
+    },
+    {
+        "name": "Quartz Mine",
+        "population": 5,
+    },
+    {
+        "name": "Lapis Lazuli Mine",
+        "population": 5,
+    },
+    {
+        "name": "Redstone Mine",
+        "population": 5,
+    },
+    {
+        "name": "Glowstone Mine",
+        "population": 5,
+    },
+    {
+        "name": "Ancient Tomes Excavator",
+        "population": 5,
+    },
+    {
+        "name": "Sugar Plantation",
+        "population": 5,
+    },
+    {
+        "name": "Wine Vineyard",
+        "population": 5,
+    },
+    {
+        "name": "Tea Plantation",
+        "population": 5,
+    },
+    {
+        "name": "Oil Well",
+        "population": 5,
+    },
+    {
+        "name": "Fur Trader",
+        "population": 5,
+    },
+    {
+        "name": "Iron Mine",
+        "population": 5,
+    },
+    {
+        "name": "Lumber Camp",
+        "population": 5,
+    },
+    {
+        "name": "Horse Farm",
+        "population": 5,
+    },
+    {
+        "name": "Potters",
+        "population": 5,
+    },
+    {
+        "name": "Coal Mine",
+        "population": 5,
+    },
+    
     
     
 ]
