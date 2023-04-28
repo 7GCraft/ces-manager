dd5762d4e3b12236cdf6ceb1d31997554dd30087
DeathWish99 <kevintrisnadi@gmail.com>
2023-04-28 10:48:41 +0700

    backend migration (errors)

--- src/background.js
+++ src/background.js
@@ -3,8 +3,10 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
-// const path = require('path');
-// const ipcs = require(path.join(__dirname, 'backend', 'backend_files', 'handlers', 'ipcEventHandler'));
+//import path from 'path'
+// import ipcs from 'ipcEventHandler'
+const path = require('path');
+const ipcs = require('./backend/backend_files/handlers/ipcEventHandler');
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 // Scheme must be registered before the app is ready
@@ -21,8 +23,8 @@ async function createWindow() {
       
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
-      nodeIntegration: process.env.ELECTRON_NODE_INTEGRATION,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
+      nodeIntegration: true,
+      contextIsolation: false
     }
   })
 
@@ -65,7 +67,7 @@ app.on('ready', async () => {
     }
   }
   createWindow()
-  //ipcs.initializeIpcMains();
+  // ipcs.initializeIpcMains();
 })
 
 // Exit cleanly on request from parent process in development mode.
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+const path = require('path')
+module.exports = {
+  pluginOptions:{
+    electronBuilder: {
+      builderOptions:{
+        extraResources: [
+          {from: './src/backend', to:'./'}
+        ]
+      }
+    }
+  }
+}
