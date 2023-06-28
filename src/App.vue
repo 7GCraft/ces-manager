a56ce8cfa00f4e9d2a154ccd5afa5a3ae4d7c343
WryanCartie <wryancartie@gmail.com>
2023-06-28 13:22:20 +0800

    Create the main layout of the app.

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,66 @@
 <template>
   <the-landing @landed="setLanding" v-if="!isLanded"></the-landing>
-  <the-homepage v-if="isLanded"></the-homepage>
+  <!--Background Container-->
+  <div v-else  class="min-h-screen w-screen
+  bg-gray-100 p-10 px-16">
+    <!--App Container-->
+    <div class="flex flex-row text-center shadow-lg bg-white">
+      <!--Sidebar -->
+        <div class="w-1/6 flex flex-col bg-gray-300
+         text-blue-400 border border-white border-x-2 ">
+          <div class=" p-2 border-white border flex justify-center"><img src='./assets/ces_logo.png' class="h-24 hover:scale-110"/></div>
+          <div class="p-6 border-white border hover:text-white hover:bg-blue-400"><a href="">Home</a></div>
+          <div class="p-6 border-white border hover:text-white hover:bg-blue-400"><a href="">State List</a></div>
+          <div class="p-6 border-white border hover:text-white hover:bg-blue-400"><a href="">Region List</a></div>
+          <div class="p-6 border-white border hover:text-white hover:bg-blue-400"><a href="">Trade Agreement</a></div>
+          <div class="p-6 border-white border hover:text-white hover:bg-blue-400"><a href="">Resource Tiers</a></div>
+        </div>
+        <!--Content-->
+        <div class="w-5/6 flex flex-col">
+          
+          <router-view></router-view>
+        </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import TheHomepage from './pages/TheHomepage.vue'
+
 import TheLanding from './pages/TheLanding.vue';
-
-
 export default {
   name: 'App',
   components: {
 
-    TheHomepage,
     TheLanding
   },
   data(){
     return{
-      isLanded: false
+      isLanded: false,
+      date: {
+        season:null,
+        year:null
+      }
     }
   },
   methods: {
     setLanding(){
       this.isLanded = true
       localStorage.setItem('landed',true)
+    },
+    getCurrentSeason(){
+      console.log('hello')
+      window.ipcRenderer.send('General:getCurrentSeason');
+      window.ipcRenderer.once('General:getCurrentSeasonOK',(e,res)=>{
+        this.date.season = res.season
+        this.date.year = res.year
+      })
     }
   },
   mounted(){
     if(localStorage.getItem('landed')){
-      this.isLanded = false
+      this.isLanded = true
     }
+    this.getCurrentSeason()
   },
 
 
--- src/pages/TheLanding.vue
+++ src/pages/TheLanding.vue
@@ -18,7 +18,7 @@
             <h5 class="mb-8 text-3xl 
             text-darkGrayishBlue md:text-4xl font-bold">Keep track of your game data.</h5>
             <p class="max-w-3xl mx-auto 
-            leading-9 text-center md:text-xl md:mb-6">The CES manager allows you to add, modify, and delete data persistently among different sessions. This data is then saved real-time to a persistent SQL database, allowing your data to be processed and modified instantly.
+            leading-9 text-center md:text-xl mb-6">The CES manager allows you to add, modify, and delete data persistently among different sessions. This data is then saved real-time to a persistent SQL database, allowing your data to be processed and modified instantly.
             </p>
             <div class="relative flex flex-col md:flex-row md:space-x-2">
                 <div class="md:w-1/2 flex flex-col justify-evenly">
@@ -28,7 +28,7 @@
                     <img src="../assets/ces-overview2.png"/>
                 </div>
                 <div
-            class="flex flex-col mt-16 space-y-12 
+            class="flex flex-col md:mt-16 space-y-12 
             text-xl md:w-1/2 md:text-left md:pl-16"
           >
             <!--Item 1-->
@@ -36,7 +36,8 @@
               <h5 class="mb-2 text-2xl font-bold text-darkGrayishBlue">
                 Multi-Tab Management
               </h5>
-              <p class="max-w-md text-grayIshBlue">
+              <p class="max-w-3xl mx-auto 
+            leading-9  md:text-xl md:mb-6">
                Open up different tabs in the app, each for a different type of data. Multi-task between different parts of the game.
               </p>
             </div>
@@ -45,7 +46,8 @@
               <h5 class="mb-2 text-2xl font-bold text-darkGrayishBlue">
                 Complete History in Real Time
               </h5>
-              <p class="max-w-md text-grayIshBlue">
+              <p class="max-w-3xl mx-auto 
+            leading-9  md:text-xl md:mb-6">
                 Retrieve any data that is presistently stored in real-time whenever you used the application.
               </p>
             </div>
@@ -54,7 +56,8 @@
               <h5 class="mb-2 text-2xl font-bold text-darkGrayishBlue">
                 Quick Search
               </h5>
-              <p class="max-w-md text-grayIshBlue">
+              <p class="max-w-3xl mx-auto 
+            leading-9  md:text-xl ">
                 Easily search and manage your game data by following the intuitive, self-explanatory UI of the application
               </p>
             </div>
@@ -66,8 +69,8 @@
     </section>
     <section id="landing-simplification">
       <div class="max-w-6xl mx-auto text-center px-16 mb-20 pt-0">
-        <h3 class="mb-8 text-4xl font-bold text-darkGrayishBlue md:text-5xl;"> Game Mastering Made Simple</h3>
-        <p class="max-w-3xl mb-4 mx-auto leading-9 text-center md:text-xl">Never again will you be feel stressed or distraught managing a multi-layered game! Our all-in-one app will allow you to manage
+        <h3 class="mb-8 text-3xl md:text-4xl font-bold text-darkGrayishBlue md:text-5xl;"> Game Mastering Made Simple</h3>
+        <p class="max-w-3xl mb-6 mx-auto leading-9 text-center md:text-xl">Never again will you be feel stressed or distraught managing a multi-layered game! Our all-in-one app will allow you to manage
           every single aspect of your game reliably and easily. Do everything with our top of the line everything-in-one application.
         </p>
         <img src="../assets/flag.png" class="mx-auto"/>
@@ -76,7 +79,7 @@
     </section>
     <section id="landing-cta">
       <div class="max-w-6xl mx-auto text-center px-16 mb-16 pt-0">
-        <h3 class="mb-8 text-4xl font-bold text-darkGrayishBlue md:text-5xl;"> Begin your journey with CES manager now.</h3>
+        <h3 class="mb-8 text-3xl md:text-4xl font-bold text-darkGrayishBlue md:text-5xl;"> Begin your journey with CES manager now.</h3>
         <p class="max-w-3xl mx-auto leading-9 text-center md:text-xl mb-10">Press the button below to kick-start your journey with the CES manager. This button will instantly
           bring you into the main CES interface.
         </p>
