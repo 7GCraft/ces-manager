f25dd908c6c4a98bec890f22c5f4b2dd0d3466b9
WryanCartie <wryancartie@gmail.com>
2023-07-03 15:17:59 +0800

    UI change on region-list.

--- src/pages/RegionList.vue
+++ src/pages/RegionList.vue
@@ -1,12 +1,12 @@
 <template>
   <div
-    class="px-20 mx-auto w-full text-center py-24 pt-16 flex flex-col space-y-5"
+    class="px-8 mx-auto w-full text-center py-24 pt-16 flex flex-col space-y-5"
   >
   <h1 class="mb-4 text-4xl font-semibold tracking-tight leading-none ">
       Region List
     </h1>
      <!--Searchbar Container-->
-     <div class="relative  ">
+     <div class="relative mx-20  ">
       <div
         class="absolute inset-y-0 left-0 flex items-center pl-5 pointer-events-none"
       >
@@ -30,18 +30,13 @@
         type="search"
         id="default-search"
         class="block w-full p-4 pl-10 md:px-24 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder="Search Mockups, Logos..."
+        placeholder="Filter by State...."
         v-model="stateFilter"
       />
-      <button
-        type="submit"
-        class="text-white absolute right-1 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-      >
-        Search
-      </button>
+      
     </div>
     <!-- Region Table Container-->
-    <div class="flex flex-col border-2 border-gray-50 shadow-lg p-4 " v-for="state in regionData" :key="state.stateID">
+    <div class="flex flex-col border-2 border-gray-50 shadow-lg p-4 " v-for="state in availableState" :key="state.stateID">
         <h1 class="mb-4 text-2xl tracking-tight leading-none " >
             {{state.stateName}}
         </h1>
@@ -59,6 +54,16 @@ export default {
     },
     mounted(){
         console.log(this.regionData,'region')
+    },
+    data(){
+      return{
+        stateFilter:''
+      }
+    },
+    computed:{
+      availableState(){
+        return this.regionData.filter(state=>state.stateName.includes(this.stateFilter))
+      }
     }
 }
 </script>
