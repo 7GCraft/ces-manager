e5380b7ff5208d831cf92e595cd97f973cd69ca8
WryanCartie <wryancartie@gmail.com>
2023-07-04 12:11:58 +0800

    Add custom colors based on CES logo.

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   content: [],
   theme: {
+    colors:{
+      'ceruleanBlue': '#03A8E8',
+      'fernGreen': '#70C073',
+      'havelockBlue': '#558FC1',
+      'lightOlive' : '#BCB859',
+      'satinSheenGold': 'D8A633',
+      'seashellWhite': '#EDEDEE'
+    },
     extend: {
       fontFamily: {
         sans: ["Bai Jamjuree", "sans-serif"],
