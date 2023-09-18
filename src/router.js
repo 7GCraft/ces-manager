a22d98d9a96575ba7e80f92bb990f9314e78d0f4
WryanCartie <wryancartie@gmail.com>
2023-09-18 16:17:24 +0800

    Begin setuping up the route names(unfinished WIP)

--- src/router.js
+++ src/router.js
@@ -1,19 +1,50 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import HomePage from './pages/HomePage'
-import StateList from './pages/StateList'
-import RegionList from './pages/RegionList'
-import TradeAgreement from './pages/TradeAgreement'
-import ResourceTier from './pages/ResourceTier'
+import { createRouter, createWebHistory } from "vue-router";
+import HomePage from "./pages/HomePage";
+import StateList from "./pages/StateList";
+import RegionList from "./pages/RegionList";
+import TradeAgreement from "./pages/TradeAgreement";
+import ResourceTier from "./pages/ResourceTier";
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes : [
-        {path:'/', component: HomePage, props: true},
-        {path:'/state-list', component:StateList},
-        {path:'/region-list', component:RegionList, props:true},
-        {path:'/trade-agreement',component: TradeAgreement,props:true},
-        {path:'/resource-tier', component:ResourceTier, props: true}
-    ]
-})
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/home",
+      component: HomePage,
+      props: true,
+      children: [
+        { path: "/", component: WelcomePage },
+        { path: "/state-list", component: StateList },
+        { path: "/region-list", component: RegionList, props: true },
+        { path: "/trade-agreement", component: TradeAgreement, props: true },
+        { path: "/resource-tier", component: ResourceTier, props: true },
+      ],
+    },
+    {
+      path: "state/:stateId",
+      component: StateInfo,
+      props: true,
+      children: [
+        { path: "/regions", component: stateRegions },
+        { path: "/facilities", component: stateFacilities },
+        { path: "/resources", component: stateResources },
+        { path: "/trade-agreement", component: stateTrade },
+        { path: "/", component: stateGeneral },
+      ],
+    },
+    {
+      path: "/region/:regionId",
+      component: regionInfo,
+      props: true,
+      children: [
+        { path: "/development", component: regionDevelopment },
+        { path: "/corruption", component: regionCorruption },
+        { path: "/facilities", component: regionFacilities },
+        { path: "/components", component: regionComponents },
+        { path:'/',component: regionGeneral}
+      ],
+    },
+  ],
+});
 
 export default router;
