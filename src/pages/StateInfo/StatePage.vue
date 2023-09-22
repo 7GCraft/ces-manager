37c69dffb5d67eb0108b9b8c41e75d5535690065
WryanCartie <wryancartie@gmail.com>
2023-09-22 18:42:58 +0800

    Remove console.logs on stateInfo.

--- src/pages/StateInfo/StatePage.vue
+++ src/pages/StateInfo/StatePage.vue
@@ -7,15 +7,6 @@
 export default {
     mounted(){
         let stateName = this.$route.params.stateId
-        console.log('we are here',stateName)
-        console.log(this.stateInfo,'brahman')
-        setTimeout(()=>{
-            console.log(this.stateInfo,'lakasha this state info')
-            console.log(this.stateFacilities,'lakasha this facilities')
-            console.log(this.stateResources,'lakasha this resources')
-            console.log(this.stateRegions,'lakasha this regions')
-            console.log(this.stateTradeAgreements,'lakasha this trade')
-        },1000)
     },
     methods(){
 
